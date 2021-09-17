d24923305fefbca7d8ccb38d41b93b9facb51786
elijah_modestov <i.modestov@tlpt.ru>
2021-09-17 16:35:43 +0300

    Fixed tests for form components

--- src/components/UserLoginForm/UserLoginForm.spec.jsx
+++ src/components/UserLoginForm/UserLoginForm.spec.jsx
@@ -4,15 +4,12 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import {UserRegistrationFormWithAuth} from "../UserRegistrationForm/UserRegistrationForm";
 
-jest.mock('../Input/Input',
-  () => ({ Input: () => <input type="text" placeholder="some input text"/> }));
 jest.mock('../Button/Button',
   () => ({ Button: () => <button>Button text</button> }));
 
 describe('UserLoginFormWithAuth', () => {
-  it('renders correctly', async () => {
+  it('renders correctly',  () => {
     const mockStore = {
       getState: () => ({ auth: { isLoggedIn: false }}),
       subscribe: () => {},
@@ -20,17 +17,16 @@ describe('UserLoginFormWithAuth', () => {
     };
     const history = createMemoryHistory();
 
-    const { getAllByPlaceholderText, getAllByText } = render(
+    const { getByPlaceholderText, getByRole, getByText } = render(
       <Router history={history}>
         <Provider store={mockStore}>
           <UserLoginFormWithAuth/>
         </Provider>
       </Router>
     );
-    const inputs = await getAllByPlaceholderText('some input text');
-    const buttons = await getAllByText('Button text');
 
-    expect(inputs).toHaveLength(2);
-    expect(buttons).toHaveLength(1);
+    expect(getByRole('textbox')).toBeInTheDocument();
+    expect(getByText('Button text')).toBeInTheDocument();
+    expect(getByPlaceholderText('*************')).toBeInTheDocument();
   });
 });
